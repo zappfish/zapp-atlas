6085251b87a9bb2f67c87310d8fce55f89eb3f38
Kevin Schaper <kevinschaper@gmail.com>
2026-06-24 12:45:18 -0700

    update research group member role names to admin + member

--- server/src/zapp_atlas/auth/models.py
+++ server/src/zapp_atlas/auth/models.py
@@ -37,7 +37,7 @@ class ResearchGroupMember(Base):
     """ A join class between users and research groups that specifies the relationship permissions """
     id: Mapped[int] = mapped_column(Integer(), primary_key=True, autoincrement=True)
     user: Mapped[str] = relationship(foreign_keys="[OrcidIdentity.id]")
-    role: Mapped[str] = mapped_column(Enum('owner','editor'))
+    role: Mapped[str] = mapped_column(Enum('admin','member'))
     group: Mapped[int] = relationship(foreign_keys=['ResearchGroup.id'])
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=_utcnow)
     updated_at: Mapped[datetime] = mapped_column(
